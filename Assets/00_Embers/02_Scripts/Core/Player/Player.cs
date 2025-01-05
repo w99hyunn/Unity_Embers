b6eb5acecb99aa48f39d9296ae3cfb562ec54aea
Woohyun Cho <tlrmsjtm77@naver.com>
2025-01-05 23:55:45 +0900

    플레이어 스폰 안정화

--- Assets/00_Embers/02_Scripts/Core/Player/Player.cs
+++ Assets/00_Embers/02_Scripts/Core/Player/Player.cs
@@ -6,7 +6,7 @@ namespace STARTING
 {
     public class Player : NetworkBehaviour
     {
-        private bool isDestroyed = false;
+        public CharacterController characterController;
         
         public override void OnStartLocalPlayer()
         {
@@ -16,10 +16,12 @@ namespace STARTING
 
         private async void InitializePlayerPosition()
         {
-            // 동기화된 데이터 기반으로 위치 초기화
-            CmdUpdateServerPosition(Managers.Game.playerData.Position);
+            characterController.enabled = false;
+
             transform.position = Managers.Game.playerData.Position;
             Debug.Log($"Position set to {transform.position}");
+            
+            characterController.enabled = true;
             
             // 위치 지속적으로 저장
             SavePosition();
@@ -32,13 +34,6 @@ namespace STARTING
                 await Awaitable.WaitForSecondsAsync(5f);
                 Managers.Game.playerData.Position = transform.position;
             }
-        }
-
-        [Command(requiresAuthority = false)]
-        private void CmdUpdateServerPosition(Vector3 position)
-        {
-            // 서버에서 클라이언트 위치 업데이트
-            transform.position = position;
         }
 
         [Command(requiresAuthority = false)]
