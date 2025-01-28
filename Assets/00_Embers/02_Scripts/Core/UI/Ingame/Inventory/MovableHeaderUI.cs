bbf237ea70f59bf090d28745773830af0f64bcc9
Woohyun Cho <tlrmsjtm77@naver.com>
2025-01-29 01:26:59 +0900

    인게임 UI창 위치 기억하는 기능 추가

--- Assets/00_Embers/02_Scripts/Core/UI/Ingame/Inventory/MovableHeaderUI.cs
+++ Assets/00_Embers/02_Scripts/Core/UI/Ingame/Inventory/MovableHeaderUI.cs
@@ -5,8 +5,9 @@ namespace NOLDA
 {
     public class MovableHeaderUI : MonoBehaviour, IPointerDownHandler, IDragHandler
     {
-        private Transform targetTransform;
+        public string positionKey;
 
+        private Transform targetTransform;
         private Vector2 _beginPoint;
         private Vector2 _moveBegin;
 
@@ -14,8 +15,33 @@ namespace NOLDA
         {
             if(targetTransform == null)
                 targetTransform = transform.parent;
+
+            LoadPosition();
         }
         
+        private void LoadPosition()
+        {
+            if (PlayerPrefs.HasKey(positionKey + "_x"))
+            {
+                float x = PlayerPrefs.GetFloat(positionKey + "_x");
+                float y = PlayerPrefs.GetFloat(positionKey + "_y");
+                float z = PlayerPrefs.GetFloat(positionKey + "_z");
+                targetTransform.position = new Vector3(x, y, z);
+            }
+            else
+            {
+                SavePosition();
+            }
+        }
+
+        private void SavePosition()
+        {
+            PlayerPrefs.SetFloat(positionKey + "_x", targetTransform.position.x);
+            PlayerPrefs.SetFloat(positionKey + "_y", targetTransform.position.y);
+            PlayerPrefs.SetFloat(positionKey + "_z", targetTransform.position.z);
+            PlayerPrefs.Save();
+        }
+
         void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
         {
             _beginPoint = targetTransform.position;
@@ -25,6 +51,7 @@ namespace NOLDA
         void IDragHandler.OnDrag(PointerEventData eventData)
         {
             targetTransform.position = _beginPoint + (eventData.position - _moveBegin);
+            SavePosition();
         }
     }
 }
